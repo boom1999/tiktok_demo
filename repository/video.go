bd7d184a97a55a9b1de0f2f17bec979f09ab35ea
cbreakALeg <754741207@qq.com>
2023-02-11 11:54:44 +0800

    The Like module is complete and passes all tests

--- repository/video.go
+++ repository/video.go
@@ -1,1 +1,14 @@
 package repository
+
+import (
+	"time"
+)
+
+type TableVideo struct {
+	Id          int64 `json:"id"`
+	AuthorId    int64
+	PlayUrl     string `json:"play_url"`
+	CoverUrl    string `json:"cover_url"`
+	PublishTime time.Time
+	Title       string `json:"title"` //视频名，5.23添加
+}
