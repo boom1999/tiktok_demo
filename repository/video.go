df0a756bdc8c31d1999b49cf297672598d5cb721
Heisenberg <lingzhicheng1999@outlook.com>
2023-02-11 14:40:42 +0800

    Use gorm.AutoMigrate() to auto set Video table.

--- repository/video.go
+++ repository/video.go
@@ -5,10 +5,15 @@ import (
 )
 
 type TableVideo struct {
-	Id          int64 `json:"id"`
-	AuthorId    int64
-	PlayUrl     string `json:"play_url"`
-	CoverUrl    string `json:"cover_url"`
-	PublishTime time.Time
-	Title       string `json:"title"` //视频名，5.23添加
+	Id          int64     `gorm:"column:id;not null;type:bigint(20) primary key auto_increment"`
+	AuthorId    int64     `gorm:"column:author_id;not null;type:bigint(20)"`
+	PlayUrl     string    `gorm:"column:play_url;not null;type:varchar(255)"`
+	CoverUrl    string    `gorm:"column:cover_url;not null;type:varchar(255)"`
+	PublishTime time.Time `gorm:"column:publish_time;not null;type:datetime"`
+	Title       string    `gorm:"column:title;not null;type:varchar(255)"`
+}
+
+// TableName 修改映射名
+func (tableVideo TableVideo) TableName() string {
+	return "videos"
 }
