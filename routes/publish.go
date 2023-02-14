72bc61fc0a51e3f79a5c24ed33861b6ba14108ad
CaoJingkang <1533218153@qq.com>
2023-02-14 15:10:43 +0800

    add video router

--- routes/publish.go
+++ routes/publish.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"tiktok_demo/controller"
 	"tiktok_demo/middleware/jwt"
 )
 
 func Publish(r *gin.RouterGroup) {
 	publish := r.Group("/publish")
 	{
-		publish.POST("/action/", jwt.Auth())
-		publish.GET("/list/", jwt.Auth())
+		publish.POST("/action/", jwt.Auth(), controller.Publish)
+		publish.GET("/list/", jwt.Auth(), controller.PublishList)
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"tiktok_demo/controller"
 	"tiktok_demo/middleware/jwt"
 )
 
 func CollectRoutes(r *gin.Engine) *gin.Engine {
 	tiktok := r.Group("/douyin")
 	{
-		tiktok.GET("/feed/", jwt.AuthWithoutLogin())
+		tiktok.GET("/feed/", jwt.AuthWithoutLogin(), controller.Feed)
 		User(tiktok)
 		Publish(tiktok)
 		Favorite(tiktok)
